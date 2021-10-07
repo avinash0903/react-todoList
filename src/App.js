10d3b8ea98a89a4b85b530146e081a7363096811
avinash0903 <avinash.skzr@gmail.com>
2021-10-07 22:46:00 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 const App = () => {
+  const [item, setItem] = useState('');
+  function itemEvent(e) {
+    setItem(e.target.value);
+  }
   return (
     <div>
       <div className="mainDiv">
         <div className="centreDiv">
           <h1>ToDo List</h1>
           <br />
-          <input type="text" placeholder="Add a Item" />
+          <input type="text" placeholder="Add a Item" onChange={itemEvent} />
           <button>+</button>
           <ol>
-            <li>Buy Apple</li>
+            <li>{item}</li>
           </ol>
         </div>
       </div>
