d0187872a275670fc6ed4eae82a2998491fa7f7a
avinash0903 <avinash.skzr@gmail.com>
2021-10-08 23:01:43 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import './style.css';
 
 const App = () => {
   const [item, setItem] = useState('');
+  const [data, setData] = useState([]);
+
   function itemEvent(e) {
     setItem(e.target.value);
   }
+
+  const displayData = () => {
+    setData((val) => {
+      return [...val, item];
+    });
+  };
+
   return (
     <div>
       <div className="mainDiv">
@@ -13,9 +22,11 @@ const App = () => {
           <h1>ToDo List</h1>
           <br />
           <input type="text" placeholder="Add a Item" onChange={itemEvent} />
-          <button>+</button>
+          <button onClick={displayData}>+</button>
           <ol>
-            <li>{item}</li>
+            {data.map((itemval) => {
+              return <li>{itemval}</li>;
+            })}
           </ol>
         </div>
       </div>
